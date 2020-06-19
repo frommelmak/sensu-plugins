209b376998aa79bf78d6eba90e0404b6d0ac7a3e
frommelmak <frommelmak@gmail.com>
2020-06-19 07:29:42 +0200

    mongodb collection row count metric script added

---
+++ metrics-collection_row_count.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# A wrapper arround the check_mondodb.py check plugin to get row count metric from a given collection
+#
+# author: frommelmelak@gmail.com
+#
+# Graphite output format:
+#
+# hostname.collection.row_count value timestamp
+#
+
+hostname=$(hostname)
+if [ "$#" -ne 3 ]; then
+    echo "USAGE: $0 mongo_host database collection"
+    exit 1
+fi
+mongo_host=$1
+database=$2
+collection=$3
+timestamp=$(date +%s)
+
+value=$(/etc/sensu/plugins/check_mongodb.py -A row_count -d $database -c $collection -H $mongo_host -P 27017 | cut -d':' -f 2 |sed 's/^ *//g')
+echo "$hostname.$database.$collection.row_count $value $timestamp"
